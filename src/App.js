4e681768d79caabedf4fc4335a6ecbf6d72483b9
Aizaz Butt <113674411+Aizaz-Butt@users.noreply.github.com>
2025-07-10 14:29:18 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function App() {
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "2rem",
+      }}
+    >
+      My first website
+    </div>
+  );
+}
+
+export default App;
